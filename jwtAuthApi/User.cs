630a79a56f476ba6b4e0a8a5289c455a4f017000
RNyaupane <roshan.nyaupane@apexcollege.edu.np>
2023-05-19 01:26:49 +0545

    Add project files.

---
+++ jwtAuthApi/User.cs
@@ -0,0 +1,11 @@
+﻿namespace jwtAuthApi
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
